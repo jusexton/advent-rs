5124770a0cec5007ea6c99f2077b0ede4126269b
jusexton <justinsexton.dev@gmail.com>
2024-12-08 22:38:26 -0600

    Refactored display functionality.

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@
 
 mod solutions;
 mod util;
-
 use std::{fs, ops::RangeInclusive};
 
 use anyhow::{anyhow, Context, Result};
@@ -29,53 +28,69 @@ fn parse_args(args: Vec<String>) -> Result<u8> {
     }
 }
 
+fn display_results<TOne, TTwo>(day: u8, part_one: TOne, part_two: TTwo)
+where
+    TOne: std::fmt::Display,
+    TTwo: std::fmt::Display,
+{
+    display_results_with_suffix(day, part_one, part_two, "\n");
+}
+
+fn display_results_with_suffix<TOne, TTwo>(day: u8, part_one: TOne, part_two: TTwo, suffix: &str)
+where
+    TOne: std::fmt::Display,
+    TTwo: std::fmt::Display,
+{
+    println!("==== Day {day} ====");
+    println!("Part One: {part_one}");
+    println!("Part Two: {part_two}");
+    print!("{suffix}")
+}
+
 fn run_by_day(day: u8) -> Result<()> {
     let path = format!("data/d{:02}.txt", day);
     let input = &fs::read_to_string(&path).context(format!("Failed to open {path}"))?;
     match day {
-        1 => {
-            let distance = solutions::d01::list_distance(input);
-            let similarity = solutions::d01::list_similarity(input);
-            println!("== Day One ==\nDistance: {distance}\nSimilarity: {similarity}\n");
-        }
-        2 => {
-            let safe_reports = solutions::d02::count_safe_reports(input);
-            let safe_reports_with_dampener =
-                solutions::d02::count_safe_reports_with_dampener(input);
-            println!("== Day Two ==\nSafe Reports: {safe_reports}\nSafe Reports With Dampener: {safe_reports_with_dampener}\n");
-        }
-        3 => {
-            let multiplications = solutions::d03::process_memory(input);
-            let with_conditionals = solutions::d03::process_memory_with_conditionals(input);
-            println!("== Day Three ==\nMultiplications: {multiplications}\nWith Conditionals: {with_conditionals}\n");
-        }
-        4 => {
-            let xmas_count = solutions::d04::count_xmas(input);
-            let x_mas_count = solutions::d04::count_x_mas(input);
-            println!("== Day Four ==\nXmas Count: {xmas_count}\nX-Mas Count: {x_mas_count}\n");
-        }
-        5 => {
-            let valid_sum = solutions::d05::sum_valid_print_queue(input);
-            let corrected_sum = solutions::d05::sum_corrected_print_queue(input);
-            println!("== Day Five ==\nValid Print Queue Sum: {valid_sum}\nCorrected Print Queue Sum: {corrected_sum}\n");
-        }
-        6 => {
-            let patrol_locations = solutions::d06::count_patrol_locations(input);
-            // Does not work
-            // let loop_possibilities = solutions::d06::count_loop_possibilities(input);
-            println!("== Day Six ==\nPatrol Locations: {patrol_locations}\n");
-        }
-        7 => {
-            let summed_calibrations = solutions::d07::sum_calibrations(input);
-            let summed_calibrations_with_concat =
-                solutions::d07::sum_calibrations_with_concatenation(input);
-            println!("== Day Seven ==\nSummed Calibrations: {summed_calibrations}\nSummed Calibrations With Concatenation: {summed_calibrations_with_concat}\n");
-        }
-        8 => {
-            let node_count = solutions::d08::count_anti_nodes(input);
-            let node_count_with_resonance = solutions::d08::count_resonant_anti_nodes(input);
-            println!("== Day Eight ==\nAnti Node Count: {node_count}\nAnti Node Count with Resonance: {node_count_with_resonance}");
-        }
+        1 => display_results(
+            1,
+            solutions::d01::list_distance(input),
+            solutions::d01::list_similarity(input),
+        ),
+        2 => display_results(
+            2,
+            solutions::d02::count_safe_reports(input),
+            solutions::d02::count_safe_reports_with_dampener(input),
+        ),
+        3 => display_results(
+            3,
+            solutions::d03::process_memory(input),
+            solutions::d03::process_memory_with_conditionals(input),
+        ),
+        4 => display_results(
+            4,
+            solutions::d04::count_xmas(input),
+            solutions::d04::count_x_mas(input),
+        ),
+        5 => display_results(
+            5,
+            solutions::d05::sum_valid_print_queue(input),
+            solutions::d05::sum_corrected_print_queue(input),
+        ),
+        6 => display_results(
+            6,
+            solutions::d06::count_patrol_locations(input),
+            "INCOMPLETE",
+        ),
+        7 => display_results(
+            7,
+            solutions::d07::sum_calibrations(input),
+            solutions::d07::sum_calibrations_with_concatenation(input),
+        ),
+        8 => display_results(
+            8,
+            solutions::d08::count_anti_nodes(input),
+            solutions::d08::count_resonant_anti_nodes(input),
+        ),
         _ => {}
     }
 
