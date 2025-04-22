0da2955b7b546375c4fd21d1843446f4cf128f31
jusexton <justinsexton.dev@gmail.com>
2025-04-21 23:11:25 -0500

    Updated shell script to also run 2023 solutions.

--- run-all.sh
+++ run-all.sh
@@ -1,5 +1,11 @@
-# Get all packages from cargo metadata that match the solution pattern
 PACKAGES=$(cargo metadata --format-version 1 | jq -r '.packages[] | .name' | grep "sol_2024_")
+for package in $PACKAGES; do
+    cargo run --release --quiet -p $package
+done
+
+echo ""
+
+PACKAGES=$(cargo metadata --format-version 1 | jq -r '.packages[] | .name' | grep "sol_2023_")
 for package in $PACKAGES; do
     cargo run --release --quiet -p $package
 done
